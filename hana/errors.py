a617ee954e3a5be0cf59e0b856bb1901ef4857b3
Mayo Jordanov <mayo@oyam.ca>
2017-09-12 17:35:00 -0700

    Add plugin error class

--- hana/errors.py
+++ hana/errors.py
@@ -2,4 +2,4 @@
 class HanaError(Exception): pass
 class HanaMissingSourceDirectoryError(HanaError): pass
 class HanaMissingOutputDirectoryError(HanaError): pass
-
+class HanaPluginError(HanaError): pass
